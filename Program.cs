40253c48f91d23b2086462110579d86be52d2c97
Andres Sainz <andresmsainz@gmail.com>
2021-12-26 15:35:53 -0500

    initial commit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _15_Casting_And_Type_Conversion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int serialNo = 100;
+            string accountNo = "";
+            float anyNo = 3.5f;
+
+            //accountNo = serialNo.ToString();
+            accountNo = Convert.ToString(serialNo);
+            
+
+            Console.WriteLine("Account Number = " + accountNo);
+
+            //serialNo = Convert.ToInt32(anyNo);
+            serialNo = (int)anyNo;
+            Console.WriteLine("Serial Number = " + serialNo);
+        }
+    }
+}
